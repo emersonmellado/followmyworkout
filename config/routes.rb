bd145bc711a1059683667459f6a7f64d473be63c
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-07-30 12:20:24 -0700

    Added exercises to app

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
 
 
   devise_for :users
+
   get 'dashboard/index'
+
+  resources :users do
+    resources :exercises
+  end
+  
+
 
 
   root to: 'dashboard#index'

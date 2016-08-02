f280ee4299a9ac1dfc0d14c2c6088f892ecf9d00
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-08-02 16:08:56 -0700

    Added first and last name to signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
 
-
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations'}
 
   get 'dashboard/index'
 
   resources :users do
     resources :exercises
   end
-  
-
-
 
   root to: 'dashboard#index'
-
 
 end

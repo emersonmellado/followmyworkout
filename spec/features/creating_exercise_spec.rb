901bc55dacce64e8eca55c6acde4c878a838f0bb
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-08-02 11:19:53 -0700

    Added destroy action

--- spec/features/creating_exercise_spec.rb
+++ spec/features/creating_exercise_spec.rb
@@ -20,7 +20,7 @@ RSpec.feature "Creating Exercise" do
   fill_in "Activity date", with: Date.today
     click_button "Create Exercise"
 
-    expect(page).to have_content("Exercise has been created.")
+    expect(page).to have_content("Exercise has been created")
 
     exercise = Exercise.last
     expect(page.current_path).to eq(user_exercise_path(@john, exercise))
